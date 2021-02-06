d82f51304d01b613c3572ac11d6fd292f0c7191a
fimfim0503 <fimfim0503@gmail.com>
2021-02-07 06:39:31 +0700

    migrasi tabel

---
+++ app/AttendanceDetail.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AttendanceDetail extends Model
+{
+    //
+    protected $guarded = [];
+
+    public function attendance()
+    {
+        return $this->belongsTo(Attendance::class);
+    }
+}
